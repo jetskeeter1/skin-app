a52617bc50d7eff0f4fc436e6d520589a747755e
jetskeeter <napala.dalton@gmail.com>
2024-02-03 01:45:51 +0800

    New pages
    Create the login page and two new pages for the admin page and super admin page that will become the dashboard. Right now it is just using to create the template.

---
+++ src/Login.js
@@ -0,0 +1,22 @@
+import './style/Login.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Login</h1>
+      <div className='user'>
+        <input type='text' className='username' placeholder='Username...'/>
+      </div>
+      <div className='pass'>
+        <input type='password' className='password' placeholder='Password...'/>
+      </div>
+
+      <button type='button' id='btn-login'>Login</button>
+      <a href='./pages/admin.js'>admin pages</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/admin.js
@@ -0,0 +1,7 @@
+function admin(){
+    <div className="admin_dashboard">
+        <h1>Welcome to admin dashboard</h1>
+
+        <a href="../index.js">Back to login</a>
+    </div>
+}
